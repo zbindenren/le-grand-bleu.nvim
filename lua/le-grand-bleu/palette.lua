2c513c1e08a7e68bafd00abd551edc26d497d7d9
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-10-02 10:30:34 +0200

    chore: update

---
+++ lua/le-grand-bleu/palette.lua
@@ -0,0 +1,65 @@
+--- palette variants
+---
+--- https://coolors.co/gradient-palette/f2f2f2-000000?number=10
+local variants = {
+  main =
+  {
+    white   = '#FFFFFF',
+    none    = 'NONE',
+    blue0   = '#03045E',
+    blue1   = '#3E4085',
+    blue2   = '#787BAD',
+    blue3   = '#B3B7D4',
+    blue4   = '#EDF2FB',
+    black0  = '#000000',
+    black1  = '#3D3D3D',
+    black2  = '#797979',
+    black3  = '#B6B6B6',
+    black4  = '#F2F2F2',
+    rose0   = '#fb6f92',
+    rose1   = '#ff8fab',
+    rose2   = '#ffb3c6',
+    rose3   = '#ffc2d1',
+    rose4   = '#ffe5ec',
+    green0  = '#036666',
+    green1  = '#3E8C89',
+    green2  = '#7AB3AB',
+    green3  = '#B5D9CE',
+    green4  = '#F0FFF0',
+    yellow0 = '#FFBF00',
+    yellow1 = '#FFCF38',
+    yellow2 = '#FFDF70',
+    yellow3 = '#FFEFA8',
+    yellow4 = '#FFFFE0',
+  },
+}
+
+local palette = variants.main
+
+return palette
+
+
+-- local variants = {
+--   main = { -- main palette
+--     yellow = '#FFFAC2',
+--     teal1 = '#5DE4C7',
+--     teal2 = '#5FB3A1',
+--     teal3 = '#42675A',
+--     blue1 = '#89DDFF',
+--     blue2 = '#ADD7FF',
+--     blue3 = '#91B4D5',
+--     blue4 = '#7390AA',
+--     pink1 = '#FAE4FC',
+--     pink2 = '#FCC5E9',
+--     pink3 = '#D0679D',
+--     blueGray1 = '#A6ACCD',
+--     blueGray2 = '#767C9D',
+--     blueGray3 = '#506477',
+--     background1 = '#303340',
+--     background2 = '#1B1E28',
+--     background3 = '#171922',
+--     text = '#E4F0FB',
+--     white = '#FFFFFF',
+--     none = 'NONE',
+--   },
+-- }
